8179aa8c2e4d4eb21e85b4358fbcf8f03fc05046
Jerome Jose <100739379+jjos425@users.noreply.github.com>
2024-07-28 00:30:28 +1200

    Style changes and added Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import "./App.css"
+import Navbar from"./Components/Navbar/Navbar"
+import Main from "./Components/Main/Main"
+import Home from "./Components/Home/Home"
+import Footer from "./Components/Footer/Footer"
 
 const App = () => {
     return(
-        <div>App</div>
+        <>
+        <Navbar/>
+        <Home/>
+        <Main/>
+        <Footer/>
+        </>
     )
 }
 export default App
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './navbar.css'
+import { MdOutlineCardTravel } from "react-icons/md";
 
 const Navbar = () => {
     return(
-        <div>Navbar</div>
+        <section className="navSection">
+            <header className="header flex">
+                <div className="logo">
+                    <a href="h" className="logo flex">
+                        <h1><MdOutlineCardTravel className="icon"/>Travel</h1>
+                    </a>
+                </div>
+            </header>
+        </section>
     )
 }
 
